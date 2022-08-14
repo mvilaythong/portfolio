4a03fbdb5dde6b547bef250bd5e3c24dd4ccce69
Michael Vilaythong <michael.vilaythong@gmail.com>
2022-08-14 11:55:31 -0500

    adding Actions styling

--- reddit-clone/components/feed/Actions.js
+++ reddit-clone/components/feed/Actions.js
@@ -1,9 +1,18 @@
 import { AnnotationIcon, GiftIcon, ShareIcon } from "@heroicons/react/outline"
 
+const style = {
+  icon: 'h-4 w-4',
+  iconContainer: ''
+}
+
 const Actions = () => {
     return (
         <div>
-            <h1>Actions</h1>
+          <div className={style.iconContainer}>
+            <AnnotationIcon className={style.icon}/>
+            <GiftIcon className={style.icon}/>
+            <ShareIcon className={style.icon}/>
+          </div>
         </div>
     )
 }
