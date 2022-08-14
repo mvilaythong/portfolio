fc2474ecb55a9c06cfe352f7ce7fd0120a09943c
Michael Vilaythong <michael.vilaythong@gmail.com>
2022-08-14 11:39:17 -0500

    added Vote component

--- reddit-clone/components/feed/Vote.js
+++ reddit-clone/components/feed/Vote.js
@@ -1,12 +1,21 @@
 import { UpvoteIcon } from '../common/UpvoteIcon'
 import { DownvoteIcon } from '../common/DownvoteIcon'
 
+const style = {
+    votes: 'py-1 text-xs font-bold text-white',
+    wrapper: 'flex flex-col items-center'
+}
+
 const Vote = () => {
+    const upvotes = 10
+    const downvotes = 5
     return (
-        <div>
+        <div className={style.wrapper}>
             <button>
                 <UpvoteIcon />
             </button>
+
+            <p className={style.votes}>{upvotes - downvotes}</p>
 
             <button>
                 <DownvoteIcon />
