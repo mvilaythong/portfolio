681cd314c59db69419a017d9108362dd0a1e2ce9
Michael Vilaythong <michael.vilaythong@gmail.com>
2022-08-21 09:48:40 -0500

    added share icons

--- reddit-clone/components/feed/Actions.js
+++ reddit-clone/components/feed/Actions.js
@@ -2,16 +2,24 @@ import { AnnotationIcon, GiftIcon, ShareIcon } from "@heroicons/react/outline"
 
 const style = {
   icon: 'h-4 w-4',
-  iconContainer: ''
+  iconContainer: 'flex items-center, space-x-1, text-[#818314]',
+  wrapper: 'flex items-center, space-x-4 pt-2'
 }
 
 const Actions = () => {
     return (
-        <div>
+        <div className={style.wrapper}>
           <div className={style.iconContainer}>
             <AnnotationIcon className={style.icon}/>
+            <span className="text-xs">19 comments</span>
+          </div>
+          <div className={style.iconContainer}>
             <GiftIcon className={style.icon}/>
+            <span className="text-xs">Awards</span>
+          </div>
+          <div className={style.iconContainer}>
             <ShareIcon className={style.icon}/>
+            <span className="text-xs">Shares</span>
           </div>
         </div>
     )
